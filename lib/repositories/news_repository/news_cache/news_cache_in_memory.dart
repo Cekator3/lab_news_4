fb0ae7eb3d79698a0c62a42ce196c3409168b070
cekator3 <hamsterdreams@inbox.ru>
2024-05-07 16:01:04 +0500

    ...

--- lib/repositories/news_repository/news_cache/news_cache_in_memory.dart
+++ lib/repositories/news_repository/news_cache/news_cache_in_memory.dart
@@ -3,15 +3,17 @@ import '../DTO/news_details.dart';
 /// A subsystem for interacting with news data stored in memory
 class NewsCacheInMemory
 {
+  List<NewsDetails> _news = [];
+
   /// Retrieves all news
 	List<NewsDetails> getAll()
   {
-    // ...
+    return _news;
   }
 
   /// Replaces all news
 	void set(List<NewsDetails> news)
   {
-    // ...
+    _news = news;
   }
 }
