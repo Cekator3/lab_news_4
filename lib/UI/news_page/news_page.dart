32d3813d3a702306bb1d0678bccf521a0ab38a9f
cekator3 <hamsterdreams@inbox.ru>
2024-05-08 23:09:29 +0500

    created form (UI only)

--- lib/UI/news_page/news_page.dart
+++ lib/UI/news_page/news_page.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: curly_braces_in_flow_control_structures
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 import 'package:lab_news_4/repositories/enums/news_channel.dart';
 import 'package:lab_news_4/repositories/news_repository/DTO/news_list_item.dart';
 import 'package:lab_news_4/repositories/news_repository/errors/find_news_errors.dart';
@@ -22,9 +24,10 @@ class NewsPage extends StatefulWidget
 class NewsPageState extends State<NewsPage>
 {
   List<NewsListItem> _newsList = [];
+  final _searchQueryController = TextEditingController();
   String? _searchQuery;
-  DateTime? _searchFrom;
-  DateTime? _searchTo;
+  DateTime? _searchFrom = DateTime.now();
+  DateTime? _searchTo = DateTime.now();
   bool _searchIgnoreWatchedNews = false;
 
   @override
@@ -33,10 +36,64 @@ class NewsPageState extends State<NewsPage>
     return Scaffold(
       appBar: AppBar(
         title: const Text('Новости'),
-        actions: [
-        ],
       ),
-      // body:
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: 30,
+          right: 30,
+          top: 0,
+          bottom: 0
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _searchQueryController,
+              decoration: const InputDecoration(
+                hintText: 'Поиск...'
+              ),
+            ),
+            const SizedBox(height: 10),
+
+            // Filters
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Text("С: ${_searchFrom?.toString().substring(0, 10)}")
+                ),
+                const SizedBox(width: 20),
+                TextButton(
+                  onPressed: () {},
+                  child: Text("До: ${_searchFrom?.toString().substring(0, 10)}")
+                ),
+              ],
+            ),
+            CheckboxListTile(
+              title: const Text(
+                'Только не просмотренные',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16
+                ),
+              ),
+              value: _searchIgnoreWatchedNews,
+              onChanged: (value) {
+                setState(() {
+                  _searchIgnoreWatchedNews = !_searchIgnoreWatchedNews;
+                });
+              },
+              controlAffinity: ListTileControlAffinity.leading,
+            ),
+
+            // Submit button
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Поиск'),
+            )
+          ],
+        ),
+      )
     );
   }
 }
