2c66bedd881365cdad2543b3d334ed8553a199bf
cekator3 <hamsterdreams@inbox.ru>
2024-05-09 14:46:57 +0500

    ...

--- lib/UI/news_page/news_page.dart
+++ lib/UI/news_page/news_page.dart
@@ -1,15 +1,12 @@
 // ignore_for_file: curly_braces_in_flow_control_structures
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:lab_news_4/repositories/enums/news_channel.dart';
 import 'package:lab_news_4/repositories/news_repository/DTO/news_list_item.dart';
 import 'package:lab_news_4/repositories/news_repository/errors/find_news_errors.dart';
+import 'package:lab_news_4/repositories/news_repository/errors/update_news_errors.dart';
 import 'package:lab_news_4/repositories/news_repository/news_repository.dart';
 import 'package:lab_news_4/repositories/news_repository/view_models/find_news_view_model.dart';
-
-import 'widgets/news_list.dart';
 
 class NewsPage extends StatefulWidget
 {
@@ -26,9 +23,42 @@ class NewsPageState extends State<NewsPage>
   List<NewsListItem> _newsList = [];
   final _searchQueryController = TextEditingController();
   String? _searchQuery;
-  DateTime? _searchFrom = DateTime.now();
-  DateTime? _searchTo = DateTime.now();
+  DateTime? _searchFrom;
+  DateTime? _searchTo;
   bool _searchIgnoreWatchedNews = false;
+
+  Future<void> _performSearch() async
+  {
+    final news = NewsRepository();
+    await news.init();
+    await news.synchronize(UpdateNewsErrors());
+    final search = FindNewsViewModel(
+      _searchQuery,
+      _searchFrom,
+      _searchTo,
+      _searchIgnoreWatchedNews
+    );
+    final errors = FindNewsErrors();
+    List<NewsListItem> newsList = news.find(widget.channel, search, errors);
+
+    if (errors.hasAny())
+    {
+      /// TODO error handling
+      return;
+    }
+
+    setState(()
+    {
+      _newsList = newsList;
+    });
+  }
+
+  @override
+  void initState()
+  {
+    super.initState();
+    _performSearch();
+  }
 
   @override
   Widget build(BuildContext context)
@@ -60,12 +90,12 @@ class NewsPageState extends State<NewsPage>
               children: [
                 TextButton(
                   onPressed: () {},
-                  child: Text("С: ${_searchFrom?.toString().substring(0, 10)}")
+                  child: Text("С: ${_searchFrom?.toString().substring(0, 10) ?? "дата начала"}")
                 ),
                 const SizedBox(width: 20),
                 TextButton(
                   onPressed: () {},
-                  child: Text("До: ${_searchFrom?.toString().substring(0, 10)}")
+                  child: Text("До: ${_searchFrom?.toString().substring(0, 10) ?? "дата конца"}")
                 ),
               ],
             ),
